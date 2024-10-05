3b03211adf876c6750b915d8bb840e363473091b
ayano-0110 <gd.clbom@gmail.com>
2024-10-05 05:56:46 +0000

    mynewsをコミット

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 
@@ -35,3 +35,7 @@ Route::controller(ProfileController::class)->group(function() {
 Route::controller(ProfileController::class)->group(function() {
     Route::get('admin/profile/edit', 'edit');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
